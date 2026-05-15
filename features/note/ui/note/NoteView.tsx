e9459f75bbabd1197cc52dbfc02ee4f3f4ff197e
Alisher Askar <alisher.askar.work@gmail.com>
2026-05-15 23:47:21 +0500

    feat(note): add form

--- features/note/ui/note/NoteView.tsx
+++ features/note/ui/note/NoteView.tsx
@@ -1,13 +1,15 @@
 import { ChevronLeft } from 'lucide-react';
+import { useState } from 'react';
 
 interface NoteViewProps {
   onBack: () => void;
 }
 
 export const NoteView = ({ onBack }: NoteViewProps) => {
+  const [content, setContent] = useState('');
   return (
     <section>
-      <header className="grid w-full grid-cols-3 items-center">
+      <header className="mb-4 grid w-full grid-cols-3 items-center">
         <button
           type="button"
           autoFocus
@@ -20,7 +22,19 @@ export const NoteView = ({ onBack }: NoteViewProps) => {
 
         <h1 className="text-center text-lg font-bold">Note</h1>
       </header>
-      <form></form>
+      <form>
+        <textarea
+          name="content"
+          id="content"
+          placeholder="Thoughts..."
+          value={content}
+          onChange={e => setContent(e.target.value)}
+          className="border-border w-full border p-2"
+        ></textarea>
+        <button className="bg-primary mx-auto rounded-2xl px-6 py-2">
+          Save
+        </button>
+      </form>
     </section>
   );
 };
