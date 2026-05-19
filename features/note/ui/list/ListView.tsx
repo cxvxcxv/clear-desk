3d826fe8297dd169c351d3e2e7b00b589528d54e
Alisher Askar <alisher.askar.work@gmail.com>
2026-05-19 23:47:29 +0500

    feat(note): display notes

--- features/note/ui/list/ListView.tsx
+++ features/note/ui/list/ListView.tsx
@@ -1,12 +1,21 @@
 import { Plus } from 'lucide-react';
 
+import { NoteCard } from './NoteCard';
+import { useNotes } from '@/entities/note';
+
 interface ListViewProps {
   openNoteView: () => void;
 }
 
 export const ListView = ({ openNoteView }: ListViewProps) => {
+  const { notes } = useNotes();
   return (
     <section className="relative h-full w-full">
+      {notes.length ? (
+        notes.map(note => <NoteCard key={note.id} note={note} />)
+      ) : (
+        <p>No notes</p>
+      )}
       <button
         onClick={openNoteView}
         className="text-foreground bg-primary absolute right-0 bottom-0 flex items-center justify-center rounded-full p-1"
